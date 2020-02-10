e503ea9f4308281ff56235d6846ac8160c9abc5c
kks76369 <60583217+kks76369@users.noreply.github.com>
2020-02-09 21:15:41 -0600

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,33 @@
+//Variable for First Name
+let firstName = prompt(`What is your first name?`)
+let firstName1 = firstName.charAt(0).toUpperCase() + firstName.slice(1)
+
+//Variable for person's State
+let personState = prompt(`What is the state abbreviation that you are from?`)
+let personState1 = personState.toUpperCase()
+
+//Vraiable for person's Temperature
+let personTemp = parseInt(prompt(`What is your temperature (in Farenheit)?`))
+
+//Message notifying the user of the data they entered into the system as a nice message
+let totalArray = [firstName.charAt(0).toUpperCase() + firstName.slice(1), personState.toUpperCase(), personTemp]
+NSB.MsgBox(`Dear ${firstName1}, you are from ${personState1} and have a temperature of ${personTemp} degrees Farenheit. Have a nice day!!`)
+console.log(totalArray);
+
+//Message based in imputs telling them what they should wear 
+switch (personTemp, personState1) {
+case (personTemp < 32 && personState1 === 'NE'):
+    NSB.MsgBox(`Wear a warm coat, hat, scarf and gloves.`)
+    break;
+case (personTemp >= 32 && personTemp < 50 && personState1 === 'NE'):
+    NSB.MsgBox(`Wear a warm coat but you won't need a hat, scarf or gloves`)
+    break;
+case (personTemp >= 32 && personTemp < 50 && personState1 === 'FL'):
+    NSB.MsgBox(`Wear your warmest coat, a warm hat, a scarf, and warm gloves.`)
+    break;
+case (personTemp >= 50 && personTemp < 70 && personState1 === 'FL'):
+    NSB.MsgBox(`wear a warm coat, hat and gloves. Maybe a scarf too.`) 
+    break;
+default:
+    NSB.MsgBox(`I do not know what you should wear.`) 
+} 
